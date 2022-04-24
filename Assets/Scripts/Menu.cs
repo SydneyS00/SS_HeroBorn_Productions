1143e842f0cd71384947152db42c0d0e59e807c9
SydneyS00 <sydschmitz03@outlook.com>
2022-04-24 14:50:07 -0700

    FIXED BUILD

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -5,8 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    //Starts the game//
     public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    //Goes to the credit section//
+    public void GetCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    //Settings button//
+
 }
